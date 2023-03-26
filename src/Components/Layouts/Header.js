b1a6735b921d50cac68ba297480f72b820fb78af
Usama Qazi <usamyharis@yahoo.com>
2023-03-26 15:30:16 +0500

    Updated Header.js

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from "../../assets/logo/c.png";
+import logo2 from '../../assets/logo/x2.png';
 
 function Header() {
     const [isOpen, setOpen] = useState(false);
@@ -10,7 +10,7 @@ function Header() {
         <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
                 <a href="/" className="flex items-center">
-                    <img src={logo} className="h-6 mr-3 sm:h-9" alt="Logo" />
+                    <img src={logo2} alt="Logo" height="100px" width="100px" />
                 </a>
                 <button data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
@@ -19,30 +19,30 @@ function Header() {
                 <div id="mega-menu-full" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
                     <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Properties</a>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 dark:text-gray-400 md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-emerald-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Properties</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Plot Finder</a>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 dark:text-gray-400 md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-emerald-500 md:dark:hover:bg-transparent dark:border-gray-700">Plot Finder</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Area Guides</a>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 dark:text-gray-400 md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-emerald-500 md:dark:hover:bg-transparent dark:border-gray-700">Area Guides</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 dark:text-gray-400 md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-emerald-500 md:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Maps</a>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 dark:text-gray-400 md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-emerald-500 md:dark:hover:bg-transparent dark:border-gray-700">Maps</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Tools</a>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 dark:text-gray-400 md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-emerald-500 md:dark:hover:bg-transparent dark:border-gray-700">Tools</a>
                         </li>
                         <li>
-                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Forum</a>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 dark:text-gray-400 md:dark:hover:text-emerald-500 dark:hover:bg-gray-700 dark:hover:text-emerald-500 md:dark:hover:bg-transparent dark:border-gray-700">Forum</a>
                         </li>
                     </ul>
                     <div className="dropdown">
                         <button
-                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
+                            className="text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
                             onClick={handleDropDown}
                         >
                             Language
